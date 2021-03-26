9537aba38175bb3b75681efdd93db6927bfbe94e
Lukasz Kokot <lkokot@kumojin.com>
2021-03-26 11:39:20 -0400

    Add models and repositories

---
+++ pkg/models/user.go
@@ -0,0 +1,17 @@
+package models
+
+// User represents a single user
+type User struct {
+	ID        string `db:"id" json:"id"`
+	Email     string `db:"email" json:"email"`
+	FirstName string `db:"first_name" json:"firstName"`
+	LastName  string `db:"last_name" json:"lastName"`
+	AvatarURL string `db:"avatar_url" json:"avatarURL"`
+}
+
+// NewUser is for registering a new user
+type NewUser struct {
+	Email     string `json:"email"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
---
+++ pkg/storage/repositories.go
@@ -0,0 +1,19 @@
+package storage
+
+import (
+	"io"
+
+	"github.com/LukaszKokot/go-clean-template/pkg/models"
+)
+
+// UserRepository is for interacting with users
+type UserRepository interface {
+	FindByID(userID string) (*models.User, error)
+	Create(user models.User) error
+	Update(user models.User) error
+}
+
+// UserAvatarRepository is for interacting with user avatars (blobs)
+type UserAvatarRepository interface {
+	CreateOrUpdate(userID string, blob io.Reader) error
+}
